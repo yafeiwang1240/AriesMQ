12a264965d003027d2220bbe87532c92bca87c74
wangyafei <364968487@qq.com>
2019-07-26 18:06:38 +0800

    新建AriesMQ 轻量级消息中间件

---
+++ src/main/java/com/github/yafei1240/aries/App.java
@@ -0,0 +1,10 @@
+package com.github.yafei1240.aries;
+
+/**
+ * Hello World
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
